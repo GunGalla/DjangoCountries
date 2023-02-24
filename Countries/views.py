6757e340089cb2f8b665da62f801633aa3c559e8
Iaroslav <GunGalla@gmail.com>
2023-02-24 19:39:47 +0200

    All tasks completed

--- Countries/views.py
+++ Countries/views.py
@@ -1,9 +1,12 @@
 """Django views file."""
 from django.core.exceptions import ObjectDoesNotExist
+from django.core.paginator import Paginator
 from django.http import HttpResponseNotFound
 from django.shortcuts import render
 
 from .models import Country, Language
+
+ALPHABET = 'ABCDEFGHIGKLMNOPQRSTUVWXYZ'
 
 
 def index(request):
@@ -14,7 +17,13 @@ def index(request):
 def countries_list(request):
     """View to show all countries.txt."""
     countries = Country.objects.all().order_by('name')
-    return render(request, 'countries_list.html', context={'countries': countries})
+    paginator = Paginator(countries, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'countries_list.html', context={
+        'page_obj': page_obj,
+        'alphabet': ALPHABET,
+    })
 
 
 def country(request, country_name):
@@ -25,6 +34,12 @@ def country(request, country_name):
         return HttpResponseNotFound(f"Country, named {country_name} not found :(")
     context = {'country': country}
     return render(request, 'country.html', context)
+
+
+def country_by_letter(request, letter):
+    countries = Country.objects.filter(name__startswith=letter)
+    context = {'countries': countries, 'letter': letter}
+    return render(request, 'letter.html', context)
 
 
 def languages_list(request):
--- DjangoCountries/urls.py
+++ DjangoCountries/urls.py
@@ -5,6 +5,7 @@ from Countries import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('countries-list/countries_by_letter/<str:letter>/', views.country_by_letter, name='letter'),
     path('countries-list/<str:country_name>/', views.country, name='distinct_country'),
     path('countries-list/', views.countries_list, name='countries_list'),
     path('languages/<str:language_name>/', views.language, name='distinct_language'),
